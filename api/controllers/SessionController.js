8c5adcd49727edd2ee993477a3ee82359e546866
Jaime BERNABE <jbernabe@e2r9p12.42.fr>
2015-11-10 18:29:14 +0100

    fwk_1 source

---
+++ api/controllers/SessionController.js
@@ -0,0 +1,31 @@
+/**
+ * SessionController
+ *
+ * @description :: Server-side logic for managing Sessions
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `SessionController.login()`
+   */
+  login: function (req, res) {
+    return res.json({
+      todo: 'login() is not implemented yet!'
+    });
+  },
+
+
+  /**
+   * `SessionController.logout()`
+   */
+  logout: function (req, res) {
+    return res.json({
+      todo: 'logout() is not implemented yet!'
+    });
+  }
+};
+
